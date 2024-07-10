1999a749acce28a2d63c5e880bcf332b02236e9e
Soyagvs <agustinmartinezyt@gmail.com>
2024-07-10 17:50:49 -0300

    add component footer

---
+++ src/components/layouts/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  const today = new Date()
+  const year = today.getFullYear()
+  return (
+    <footer className='footer-container'>
+      <p>all right reserved | {year}</p>
+    </footer>
+  )
+}
+
+export default Footer
