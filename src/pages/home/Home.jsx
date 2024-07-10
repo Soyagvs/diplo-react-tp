c81ec1778e861d56484f45de2950dcca380753c0
Soyagvs <agustinmartinezyt@gmail.com>
2024-07-10 17:54:28 -0300

    add page home

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,28 @@
+import BgVideo from "../../assets/video.webm"
+import "./Home.css"
+const Home = () => {
+  return (
+    <main>
+      <div className="home-container">
+        <h1 className="home-h1">Dale vida a tus emprendimientos desde la web</h1>
+        <p className="home-p">
+          Comunicate con nosotros y pon en meta tus proyectos personales o dale mas conocimiento a tu empresa familiar.
+        </p>
+        <div className="home-buttons">
+          <button>Get started</button>
+          <button className="gold">Learn more</button>
+        </div>
+        <video
+          className="home-video"
+          src={BgVideo}
+          autoPlay
+          loop
+          muted
+        />
+      </div>
+
+    </main>
+  )
+}
+
+export default Home
