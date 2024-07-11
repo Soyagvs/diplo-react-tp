33bb91715fb2240b298e5752aeae4017fbac1e13
Soyagvs <agustinmartinezyt@gmail.com>
2024-07-11 01:01:54 -0300

    add component contact page

--- src/pages/contacts/Contacts.jsx
+++ src/pages/contacts/Contacts.jsx
@@ -1,10 +1,73 @@
 import React from 'react'
+import "./Contacts.css"
 
 const Contacts = () => {
   return (
-    <div>
-      contact
-    </div>
+    <main className='contact-main'>
+      <div className='contact-primary'>
+        <div className='contact-text'>
+          <h2 className='contact-h2'>Contact Us</h2>
+          <p className='contact-p'>Por favor, complete el formulario y nos pondremos en contacto con usted a la brevedad a través de su correo electrónico.</p>
+        </div>
+        <div className='others'>
+          <div className='contact-grid'>
+
+            <div className='grid-item'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="svg">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+              </svg>
+
+              <h3>Atencion</h3>
+              <p className='text-p'>Lun a Sab - 8hs a 17hs</p>
+            </div>
+            <div className='grid-item'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="svg">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3" />
+              </svg>
+
+              <h3>Email</h3>
+              <p className='text-p'>agvs@gmail.com</p>
+            </div>
+
+            <div className='grid-item'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="svg">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+              </svg>
+
+              <h3>Celular</h3>
+              <p className='text-p'>+54 9 1138192126</p>
+            </div>
+
+            <div className='grid-item'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="svg">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+              </svg>
+
+
+              <h3>Direccion</h3>
+              <p className='text-p'>Buenos Aires, AR</p>
+            </div>
+          </div>
+
+          <div className='contact-form'>
+            <div className='contact-container'>
+              <form action="" className='form'>
+                <label htmlFor="name">Nombre</label>
+                <input type="text" id='name' required autoComplete='additional-name' />
+
+                <label htmlFor="email">Email</label>
+                <input type="email" id='email' required autoComplete='additional-name'/>
+
+                <label htmlFor="message">Mensaje</label>
+                <input type="text" id='message' required/>
+              </form>
+              <button type="submit">Enviar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
