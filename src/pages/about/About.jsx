2dbf4c7f20809cef9fe92a8850026efdab49758c
Soyagvs <agustinmartinezyt@gmail.com>
2024-07-11 01:01:42 -0300

    add component about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,9 +1,20 @@
+import ImageAbout from "../../assets/img/img-about.webp"
 import "./About.css"
 const About = () => {
   return (
-    <div>
-      about
-    </div>
+    <main>
+      <div className="about-container">
+        <h2>About Us</h2>
+        <div className="about-img-text">
+          <img src={ImageAbout} alt="" className="img-about" />
+          <div className="p-container">
+            <p>
+              <span>"</span>Somos una agencia especializada en diseño web, desarrollo web, branding y marketing digital. Nuestro objetivo es transformar tus ideas en soluciones digitales impactantes y efectivas. Nos apasiona crear experiencias únicas que conecten a las marcas con su audiencia, impulsando su crecimiento y éxito en el entorno digital. <span>"</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
