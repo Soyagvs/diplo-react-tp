f69e006ea9684013448893ec4789868fb46e1dd3
Soyagvs <agustinmartinezyt@gmail.com>
2024-08-05 19:25:04 -0300

    add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import {NotFound} from './components/NotFound'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from './components/layouts/navbar/Navbar'
 import Footer from './components/layouts/footer/Footer'
+import Navbar from './components/layouts/navbar/Navbar'
 
-import Home from './pages/home/Home'
-import About from './pages/about/About'
-import Contacts from './pages/contacts/Contacts'
-import Pricing from './pages/pricing/Pricing'
+import {Home} from './pages/home/Home'
+import {About} from './pages/about/About'
+import {Contacts} from './pages/contacts/Contacts'
+import { Tasks } from './pages/tasks/Tasks'
+import { NotFound } from './components/NotFound'
+
 import './index.css'
-
 
 
 function App() {
@@ -16,15 +16,15 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      
+
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/contacts' element={<Contacts />} />
-        <Route path='/pricing' element={<Pricing />} />
-        <Route path='/*' component={<NotFound/>}/>
+        <Route path='/tasks' element={<Tasks />} />
+        <Route path='/*' component={<NotFound />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   )
 }
