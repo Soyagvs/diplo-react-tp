8615c68ee4f617d6787bff89e349043e51d3cf4b
soyagvs <agustinmartinezyt@gmail.com>
2024-08-16 03:27:52 -0300

    finish page contcat

--- src/pages/contacts/Contacts.jsx
+++ src/pages/contacts/Contacts.jsx
@@ -1,37 +1,37 @@
-import "./Contacts.css"
+import "./Contacts.css";
 
 export const Contacts = () => {
   return (
-    <main className='contact-main'>
-      <div className='contact-primary'>
-        <div className='contact-text'>
-          <h2 className='contact-h2'>Contact Us</h2>
-          <p className='contact-p'>Por favor, complete el formulario y nos pondremos en contacto con usted a la brevedad a través de su correo electrónico.</p>
-        </div>
-        <div className='others'>
+    <main className="contact-main">
+      <h2 className="contact-h2">Contactame</h2>
+      <div className="context-contacto">
+        <p className="contact-p">
+          Por favor, complete el formulario y nos pondremos en contacto con
+          usted a la brevedad a través de su correo electrónico.
+        </p>
 
+        <form action="" className="form">
+          <label htmlFor="name">Nombre</label>
+          <input
+            type="text"
+            id="name"
+            required
+            autoComplete="additional-name"
+          />
 
-          <div className='contact-form'>
-            <div className='contact-container'>
-              <form action="" className='form'>
-                <label htmlFor="name">Nombre</label>
-                <input type="text" id='name' required autoComplete='additional-name' />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            required
+            autoComplete="additional-name"
+          />
 
-                <label htmlFor="email">Email</label>
-                <input type="email" id='email' required autoComplete='additional-name' />
-
-                <label htmlFor="message">Mensaje</label>
-                <input type="text" id='message' required />
-              </form>
-              <button type="submit">Enviar</button>
-            </div>
-          </div>
-        </div>
-
+          <label htmlFor="message">Mensaje</label>
+          <textarea name="message" id="message" ></textarea>
+          <button type="submit">Enviar</button>
+        </form>
       </div>
     </main>
-  )
-
-
-}
-
+  );
+};
