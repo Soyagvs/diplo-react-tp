10ab00460c5c9fee62ce581af6aa51d00253c674
soyagvs <agustinmartinezyt@gmail.com>
2024-08-30 21:38:46 -0300

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Footer } from "./components/layouts/footer/Footer";
 import { Navbar } from "./components/layouts/navbar/Navbar";
-
 import { Home } from "./pages/home/Home";
 import { About } from "./pages/about/About";
 import { Contacts } from "./pages/contacts/Contacts";
 import { Posts } from "./pages/posts/Posts";
-
 import "./index.css";
-
 
 function App() {
   return (
